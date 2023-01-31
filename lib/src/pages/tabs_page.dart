d731c9a31e5cab176632ce78a31e251a55943153
Nico <nicocieri16@gmail.com>
2023-01-30 21:17:41 -0300

    PageView

---
+++ lib/src/pages/tabs_page.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+
+class TabsPage extends StatelessWidget {
+  const TabsPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: _Pages(),
+      bottomNavigationBar: _Navigation(),
+    );
+  }
+}
+
+class _Navigation extends StatelessWidget {
+  const _Navigation({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      currentIndex: 0,
+      items: const [
+        BottomNavigationBarItem(
+          icon: Icon(Icons.person_outline),
+          label: 'For you',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.public),
+          label: 'Headlines',
+        ),
+      ],
+    );
+  }
+}
+
+class _Pages extends StatelessWidget {
+  const _Pages({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return PageView(
+      // physics: const BouncingScrollPhysics(),
+      physics: const NeverScrollableScrollPhysics(),
+      children: [
+        Container(
+          color: Colors.red,
+        ),
+        Container(
+          color: Colors.green,
+        ),
+      ],
+    );
+  }
+}
