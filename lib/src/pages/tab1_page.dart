1cd74beaf1e10a48c2af3440fc7599b7bf360a94
Nico <nicocieri16@gmail.com>
2023-02-01 11:16:49 -0300

    Automatic keep alive

--- lib/src/pages/tab1_page.dart
+++ lib/src/pages/tab1_page.dart
@@ -3,11 +3,18 @@ import 'package:news_app/src/services/news_service.dart';
 import 'package:news_app/src/widgets/news_list.dart';
 import 'package:provider/provider.dart';
 
-class Tab1Page extends StatelessWidget {
+class Tab1Page extends StatefulWidget {
   const Tab1Page({super.key});
 
   @override
+  State<Tab1Page> createState() => _Tab1PageState();
+}
+
+class _Tab1PageState extends State<Tab1Page>
+    with AutomaticKeepAliveClientMixin {
+  @override
   Widget build(BuildContext context) {
+    super.build(context);
     final headLines = Provider.of<NewsService>(context).headLines;
 
     return Scaffold(
@@ -16,4 +23,7 @@ class Tab1Page extends StatelessWidget {
           : NewsList(headLines),
     );
   }
+
+  @override
+  bool get wantKeepAlive => true;
 }
--- lib/src/pages/tabs_page.dart
+++ lib/src/pages/tabs_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:news_app/src/pages/tab1_page.dart';
+import 'package:news_app/src/pages/tab2_page.dart';
 import 'package:provider/provider.dart';
 
 class TabsPage extends StatelessWidget {
@@ -56,11 +57,9 @@ class _Pages extends StatelessWidget {
       controller: navegationModel.pageController,
       // physics: const BouncingScrollPhysics(),
       physics: const NeverScrollableScrollPhysics(),
-      children: [
+      children: const [
         Tab1Page(),
-        Container(
-          color: Colors.green,
-        ),
+        Tab2Page(),
       ],
     );
   }
