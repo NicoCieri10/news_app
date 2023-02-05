b92a5b4ed4c05515b8f80e10d42a212cd8622e21
Nico <nicocieri16@gmail.com>
2023-02-05 14:33:13 -0300

    category button

--- lib/src/pages/tab2_page.dart
+++ lib/src/pages/tab2_page.dart
@@ -1,5 +1,6 @@
 import 'package:appsize/appsize.dart';
 import 'package:flutter/material.dart';
+import 'package:news_app/src/models/category_model.dart';
 import 'package:news_app/src/services/news_service.dart';
 import 'package:provider/provider.dart';
 
@@ -36,17 +37,47 @@ class _CategoriesList extends StatelessWidget {
       scrollDirection: Axis.horizontal,
       itemCount: categories.length,
       itemBuilder: (BuildContext context, int index) {
+        final name = categories[index].name;
+
         return Padding(
           padding: EdgeInsets.all(8.sp),
           child: Column(
             children: [
-              Icon(categories[index].icon),
+              _CategoryButton(categories[index]),
               SizedBox(height: 5.sp),
-              Text(categories[index].name),
+              Text('${name[0].toUpperCase()}${name.substring(1)}'),
             ],
           ),
         );
       },
+    );
+  }
+}
+
+class _CategoryButton extends StatelessWidget {
+  const _CategoryButton(
+    this.category,
+  );
+
+  final Category category;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {},
+      child: Container(
+        width: 40.sp,
+        height: 40.sp,
+        margin: EdgeInsets.symmetric(horizontal: 10.sp),
+        decoration: const BoxDecoration(
+          shape: BoxShape.circle,
+          color: Colors.white,
+        ),
+        child: Icon(
+          category.icon,
+          color: Colors.black54,
+        ),
+      ),
     );
   }
 }
