e783cfb01384beea878fa28444bb25d4a7fe9a2c
Nico <nicocieri16@gmail.com>
2023-02-05 14:20:54 -0300

    widgets.dart

--- lib/src/pages/tab1_page.dart
+++ lib/src/pages/tab1_page.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:news_app/src/services/news_service.dart';
-import 'package:news_app/src/widgets/news_list.dart';
+import 'package:news_app/src/widgets/widgets.dart';
 import 'package:provider/provider.dart';
 
 class Tab1Page extends StatefulWidget {
